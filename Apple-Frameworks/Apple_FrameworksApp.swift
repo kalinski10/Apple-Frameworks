3ec05311eb39e94910a630d933cbeb63a74ea493
kalin balabnov <kalin_dude10@live.com>
2020-12-20 21:06:30 +0000

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Kalin Balabanov on 20/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
